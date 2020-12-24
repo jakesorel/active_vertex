ef0e52fc5f136f9194f86030bf198666417555a1
jakesorel <Coexist-97>
2020-12-24 16:36:07 +0000

    Mammary gland model included.

---
+++ run_job_fromfile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=0:30:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH -J "AV_jakecs"   # job name
+#SBATCH --output=output.out
+#SBATCH --error=output.out
+
+source activate apical_domain
+
+python voronoi_model/SPV_from_file_cluster.py "$1" "$2" "$3"
